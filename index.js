8b627e3e6823d24a5315d25725cdcbbb6d348521
codermahbub20 <mkrefat5@gmail.com>
2023-11-05 22:10:12 +0600

    specific id to search assignment implemented

--- index.js
+++ index.js
@@ -1,15 +1,88 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
+
+// onlineGroupStudy
+// L3cWOC1YmDlfk40b
+
+
+
+
+
+
+
+const uri = `mongodb+srv://onlineGroupStudy:L3cWOC1YmDlfk40b@cluster0.rarr4yf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const createAssignmnetCollection = client.db('onlineStudy').collection('createAssignment');
+
+
+
+        // create assignment related api
+
+        app.post('/createAssignment', async (req, res) => {
+            const user = req.body;
+            const result = await createAssignmnetCollection.insertOne(user);
+            res.send(result)
+
+        })
+
+
+        app.get('/createAssignment', async (req, res) => {
+            const result = await createAssignmnetCollection.find().toArray();
+            res.send(result)
+        })
+
+        // update Related api
+
+        app.get('/createAssignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await createAssignmnetCollection.findOne(query)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 
 app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('online group study!')
+    res.send('online group study!')
 })
 
 app.listen(port, () => {
-  console.log(`Online group study listening on port ${port}`)
+    console.log(`Online group study listening on port ${port}`)
 })
