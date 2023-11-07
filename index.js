eb8a4846fa23c1e30405cd64e177844b56385a4f
codermahbub20 <mkrefat5@gmail.com>
2023-11-08 02:21:29 +0600

    Token successfully send

--- index.js
+++ index.js
@@ -41,11 +41,10 @@ async function run() {
 
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, '3d51ce94ce9a97b89842b35feff705b3f626b36df0146e478fafb71a228fc2df095e40959c438c293e343d1fe01412d1528baeab7305ce22d27f2787bad2c266', { expiresIn: '1h' });
+            const token = jwt.sign(user, 'process.env.ACCESS_TOKEN', { expiresIn: '1h' });
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: 'none'
+                 
             })
                 .send({ success: true })
         })
